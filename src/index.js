5f34d2f1e04aa58990ad6ae17127e92d9a9c4890
Andrey Sankovich <53842066+bANSHEE-dev@users.noreply.github.com>
2021-03-04 03:22:08 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   const open = ['(', '{', '['];
+  const close = [')', '}', ']'];
+  const stack = [];
+  
+  for(let i=0; i < str.length; i++) {
+    if(open.includes(str[i])) {
+    stack.push(str[i]);
+    } else {
+      if(close.indexOf(str[i]) === open.indexOf(stack[stack.length - 1] )){
+        stack.pop();
+      }  else {
+        return false;
+      } 
+    }
+  }   
+  return stack.length === 0;
 }
